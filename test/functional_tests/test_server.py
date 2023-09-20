3b9efb3580b87b8e25e57073b98f31e4a22510b5
zhao yh <makai87kai@outlook.com>
2023-09-20 22:22:12 +0200

    functional test finished

--- test/functional_tests/test_server.py
+++ test/functional_tests/test_server.py
@@ -4,28 +4,30 @@ from selenium import webdriver
 import time
 
 
-def test_login_selenium(validate_club):
-    driver = webdriver.Firefox()
+def test_login_and_purchase_places(validate_club):
+    driver = webdriver.Chrome()
 
     # navigate to initial url
     app_url = "http://127.0.0.1:5000/"
     driver.get(app_url)
 
     # enter email and press enter
-    input_box = driver.find_element(By.NAME, "email")
+    input_box = driver.find_element(By.NAME, 'email')
     input_box.send_keys(validate_club['email'])
     time.sleep(2)
     input_box.send_keys(Keys.RETURN)
-    time.sleep(5)
+    time.sleep(2)
 
-    # # navigate to welcome page
-    # welcome_page_url = driver.current_url
-    # driver.get(welcome_page_url)
-    # time.sleep(2)
-    # click_button = driver.find_element(By.XPATH, '/html/body/ul[2]/li[2]/a')
-    # # click_button.click()
-    # time.sleep(5)
-    # print(click_button)
-    # print(driver.current_url)
+    # welcome page, find book places hyperlink and click
+    click_button = driver.find_element(By.XPATH, '/html/body/ul/li[1]/a')
+    click_button.click()
+    time.sleep(2)
+
+    # book one place
+    input_box = driver.find_element(By.NAME, 'places')
+    input_box.send_keys(1)
+    time.sleep(2)
+    input_box.send_keys(Keys.RETURN)
+    time.sleep(2)
 
     driver.quit()
--- test/performance_test/locustfile.py
+++ test/performance_test/locustfile.py
@@ -4,8 +4,8 @@ from locust import HttpUser, task
 class ProjectPerfTest(HttpUser):
 
     @task
-    def showsummary(self, validate_club):
-        self.client.post('/showSummary', {'email': validate_club['email']})
+    def showsummary(self):
+        self.client.post('/showSummary', {'email': 'admin@irontemple.com'})
 
     @task(6)
     def login(self):
